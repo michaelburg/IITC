c97499cf06be33a7545fae7c32bf4a030f8cdbda
michaelburg <mclburg@gmail.com>
2024-05-19 11:02:36 +0300

    'new'

--- javascript/my-budget/script.js
+++ javascript/my-budget/script.js
@@ -1,158 +1,311 @@
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-totalbudget = 0;
-totalincome = 0;
-totalexpense = 0;
-data = JSON.parse(window.localStorage.getItem("data")) || {
-  incomes: {},
-  expenses: {},
-};
-loadPage();
-function loadPage() {
-  date = new Date();
-  document.getElementById("headWithDate").innerHTML = `Available budjet in ${
-    months[date.getMonth()] + " " + date.getFullYear()
-  }`;
-  for (const key in data.expenses) {
-    reduce(key, data.expenses[key].value);
-  }
-  for (const key in data.incomes) {
-    add(key, data.incomes[key].value);
-  }
+let actionElement = document.getElementById("action");
+let descriptionElement = document.getElementById("actionDescription");
+let valueElement = document.getElementById("actionValue");
+const redColor = "#F53237";
+const greenColor = "rgb(56, 178, 173)";
+const borderColor = "rgb(202, 202, 202)";
+let totalBudget = 0;
+let totalIncome = 0;
+let totalExpense = 0;
+let currentBudget = 0;
+let isPageLoad = true;
+const docBody = document.querySelector("body");
+let isDarkMode = JSON.parse(localStorage.getItem("darkModeActive")) || false;
+if (isDarkMode) {
+  colorBody();
 }
+let borderSize = toggleBorderSize();
+let transactions =
+  JSON.parse(window.localStorage.getItem("transactions")) || [];
+changeDarkModeButtons();
+addEventListenerToTextInput();
+addEventListenerToValueInput();
+addEventListenerToAction();
+handleSelectChange();
+loadPage();
+updateDarkModeIcon();
+
+function loadPage() {
+  let action;
+  document.getElementById(
+    "headWithDate"
+  ).innerText = `Available budget in ${getDateToTitle()}:`;
+  currentBudget = totalBudget;
+  transactions.forEach((transaction) => {
+    transaction["value"] < 0 ? (action = "expense") : (action = "income");
+    commitAction(
+      action,
+      transaction["description"],
+      transaction["value"],
+      transaction["timeStamp"]
+    );
+  });
+  isPageLoad = false;
+}
+
+function submitAction() {
+  let action = actionElement.value;
+  let description = descriptionElement.value;
+  let actionValue = valueElement.valueAsNumber;
+  if (validateInput(description, actionValue)) {
+    showSnackbar();
+    return;
+  }
+  if (action === "expense") actionValue *= -1;
+  time = new Date().getTime();
+  transactions.push({
+    description: description,
+    value: actionValue,
+    timeStamp: time,
+  });
+  currentBudget = totalBudget;
+  commitAction(action, description, actionValue, time);
+  descriptionElement.value = "";
+  valueElement.value = "";
+}
+
+let snackbarTimeout;
+
+function showSnackbar() {
+  let snackbarMessage = document.getElementById("snackbar");
+  snackbarMessage.classList.remove("show");
+  void snackbarMessage.offsetWidth;
+  snackbarMessage.classList.add("show");
+  if (snackbarTimeout) {
+    clearTimeout(snackbarTimeout);
+  }
+  snackbarTimeout = setTimeout(function () {
+    snackbarMessage.classList.remove("show");
+  }, 2400);
+}
+
+function commitAction(action, description, actionValue, time) {
+  actionValue < 0
+    ? (totalExpense += actionValue)
+    : (totalIncome += actionValue);
+  currentBudget = totalBudget;
+  totalBudget += actionValue;
+  createNewAction(action, description, actionValue, time);
+  setHead();
+  setExpensesPer();
+  updateLocalStorage();
+}
+function createNewAction(action, description, actionValue, time) {
+  let cancelClass = isDarkMode
+    ? "fa-solid fa-circle-xmark transactionCancel"
+    : "fa-regular fa-circle-xmark xMark transactionCancel";
+
+  let parent = document.querySelector(`.${action}Items`);
+  let newAction = document.createElement("div");
+  newAction.className = action + "Wrapper";
+  newAction.id = time;
+  newAction.innerHTML = `
+  <p class=actionName>${description}</p>
+  <div class = "transaction">
+  <p class="transactionAmount">${numberToPrint(actionValue)}</p>
+  ${action === "expense" ? `<p class="percent"></p>` : ""}
+  <p class="timeStamp">${time}</p>
+  <i class="${cancelClass}" onclick="cancel(this,${time})"></i>
+  </div>
+  `;
+  parent.appendChild(newAction);
+}
+
+function setHead() {
+  animateBudgetChange();
+  document.getElementById("totalIncome").innerText = numberToPrint(totalIncome);
+  document.getElementById("totalExpenses").innerText = numberToPrint(
+    totalExpense,
+    true
+  );
+  document.getElementById("totalPercent").innerText = `${
+    parseInt((totalExpense * 100) / totalIncome) * -1 || 0
+  }%`;
+}
+
+function animateBudgetChange() {
+  if (isPageLoad) {
+    updateBudgetDisplay(totalBudget);
+    return;
+  }
+  let from = currentBudget;
+  const to = totalBudget;
+  const numOfTimes = 47;
+  const amountToChange = (to - from) / numOfTimes;
+  const interval = 20;
+  let counter = setInterval(function () {
+    // If the budget has reached its final value, clear the interval and exit the function
+    if (from === to) {
+      clearInterval(counter);
+      return;
+    }
+    from += amountToChange;
+    // If the current budget has reached or passed the total budget, set it to the total budget
+    if (
+      (amountToChange > 0 && from >= to) ||
+      (amountToChange < 0 && from <= to)
+    ) {
+      from = to;
+    }
+    updateBudgetDisplay(from);
+  }, interval);
+}
+
+function updateBudgetDisplay(budget) {
+  let budgetElement = document.getElementById("totalBudget");
+  budgetElement.innerText = numberToPrint(budget);
+}
+
+function setExpensesPer() {
+  let expenseDiv = document.querySelectorAll(".expenseWrapper");
+  expenseDiv.forEach((div) => {
+    let timeStamp = parseInt(div.querySelector(".timeStamp").innerText);
+    const foundTransaction = transactions.find(
+      (transaction) => transaction.timeStamp === timeStamp
+    );
+    let expensePer = div.querySelector(".percent");
+    let percent =
+      parseInt((foundTransaction["value"] * 100) / totalIncome) * -1 || 0;
+    expensePer.innerText = `${percent}%`;
+  });
+}
+
+function cancel(cancelButton, timeStamp) {
+  const foundTransaction = transactions.find(
+    (transaction) => transaction.timeStamp === timeStamp
+  );
+  let actionDiv =
+    cancelButton.closest(".incomeWrapper") ||
+    cancelButton.closest(".expenseWrapper");
+  if (actionDiv === cancelButton.closest(".incomeWrapper"))
+    totalIncome -= foundTransaction["value"];
+  else if (actionDiv === cancelButton.closest(".expenseWrapper"))
+    totalExpense -= foundTransaction["value"];
+  currentBudget = totalBudget;
+
+  totalBudget -= foundTransaction["value"];
+  transactions = transactions.filter(
+    (transaction) => transaction.timeStamp !== timeStamp
+  );
+  actionDiv.remove();
+  setHead();
+  setExpensesPer();
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+function getDateToTitle() {
+  const date = new Date();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+  return month + " " + year;
+}
+function validateInput(description, actionValue) {
+  return (
+    description === "" ||
+    isNaN(actionValue) ||
+    actionValue <= 0 ||
+    transactions.hasOwnProperty(description)
+  );
+}
+function numberToPrint(number, isExpenseHead = false) {
+  let sign = "+";
+  if (number < 0 || isExpenseHead) {
+    sign = "-";
+    number = Math.abs(number);
+  }
+  const fixNum = number.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
+  return `${sign} ${fixNum}`;
+}
+function changeBorderColor() {
+  const action = document.getElementById("action").value;
+  if (action == "expense") return redColor;
+  return greenColor;
+}
+function handleSelectChange() {
+  document.getElementById("submitAction").style.color = changeBorderColor();
+}
+
+function addEventListenerToTextInput() {
+  descriptionElement.addEventListener("focus", function () {
+    descriptionElement.style.border = `${borderSize} solid ${changeBorderColor()}`;
+  });
+  descriptionElement.addEventListener("blur", function () {
+    descriptionElement.style.border = `1px solid ${borderColor}`;
+  });
+}
+
+function addEventListenerToValueInput() {
+  valueElement.addEventListener("focus", function () {
+    valueElement.style.border = `${borderSize} solid ${changeBorderColor()}`;
+  });
+
+  valueElement.addEventListener("blur", function () {
+    valueElement.style.border = `1px solid ${borderColor}`;
+  });
+}
+
+function addEventListenerToAction() {
+  actionElement.addEventListener("click", function () {
+    actionElement.style.border = `${borderSize} solid ${changeBorderColor()}`;
+  });
+  actionElement.addEventListener("blur", function () {
+    actionElement.style.border = "1px solid " + borderColor;
+  });
+}
+
 document.addEventListener("keyup", function (event) {
   if (event.key === "Enter") submitAction();
 });
-function submitAction() {
-  action = document.getElementById("action").value;
-  actionDescriptionText = document.getElementById("actionDescription");
-  actionValueText = document.getElementById("actionValue");
-  actionDescription = actionDescriptionText.value;
-  actionValue = actionValueText.valueAsNumber;
-  actionValue = Math.round(parseFloat(actionValue) * 100) / 100;
-  if (
-    actionDescription == "" ||
-    actionValue <= 0 ||
-    isNaN(actionValue) ||
-    data.incomes.hasOwnProperty(actionDescription)
-  )
-    return;
-  if (action == "add") add(actionDescription, actionValue);
-  else if (action == "reduce") reduce(actionDescription, actionValue);
-  actionDescriptionText.value = "";
-  actionValueText.valueAsNumber = "";
-}
-function add(actionDescription, actionValue) {
-  totalincome += actionValue;
-  totalbudget += actionValue;
-  parent = document.getElementById("incomes");
-  newAction = document.createElement("div");
-  newAction.id = "income";
-  newAction.innerHTML = `
-    <p id="description">${actionDescription}</p>
-    <p id="income-amount">${actionValue}</p>
-    <button id="cancelIncome" onclick="cancelIncome(this)">Cancel</button>
-  `;
-  parent.appendChild(newAction);
-  data.incomes[actionDescription] = { value: actionValue };
-  setHead();
-  setExpensesPer();
-  updateLocalStorage();
-}
-function reduce(actionDescription, actionValue) {
-  totalexpense += actionValue;
-  totalbudget -= actionValue;
-  persante = parseInt((actionValue * 100) / totalincome) || 0;
-  parent = document.getElementById("expenses");
-  newAction = document.createElement("div");
-  newAction.id = "expense";
-  newAction.innerHTML = `
-  <p id="description">${actionDescription}</p>
-  <p id="expense-amount">${actionValue}</p>
-  <p id="percent">${persante}%</p>
-  <button id="cancelExpense" onclick="cancelExpense(this)">Cancel</button>
-`;
-  parent.appendChild(newAction);
-  data.expenses[actionDescription] = { value: actionValue };
-  setHead();
-  setExpensesPer();
-  updateLocalStorage();
-}
-function setHead() {
-  budjet = document.getElementById("totalbudget");
-  if (totalbudget >= 0) {
-    budjet.style.color = "green";
-    budjet.innerText = "+" + totalbudget;
-  } else {
-    budjet.style.color = "red";
-    budjet.innerText = totalbudget;
-  }
-  document.getElementById("totalIncome").innerText = totalincome;
-  document.getElementById("totalExpenses").innerText = totalexpense;
-  document.getElementById("totalPersant").innerText = `${
-    parseInt((totalexpense * 100) / totalincome) || 0
-  }%`;
-}
-function setExpensesPer() {
-  expenseDiv = document.querySelectorAll("#expense");
-  expenseDiv.forEach((div) => {
-    description = div.querySelector("#description").innerText;
-    expense = div.querySelector("#expense-amount").innerText;
-    expensePer = div.querySelector("#percent");
-    persant = parseInt((expense * 100) / totalincome) || 0;
-    expensePer.innerText = `${persant}%`;
-  });
-  // <p id="percent">${parseInt((actionValue * 100) / income)}%</p>
-}
-function cancelIncome(btn) {
-  // const incomes = document.getElementById("incomes");
-  div = btn.parentNode;
-  cancelIncomeDescription = div.querySelector("#description").innerText;
-  cancelIncomeAmount = div.querySelector("#income-amount").innerText;
-  totalbudget -= cancelIncomeAmount;
-  totalincome -= cancelIncomeAmount;
-  setHead();
-  setExpensesPer();
-  delete data.incomes[cancelIncomeDescription];
-  updateLocalStorage();
 
-  div.remove();
+function toggleDarkMode() {
+  isDarkMode = isDarkMode === false ? true : false;
+  updateDarkModeIcon();
+  colorBody();
+  changeDarkModeButtons();
+  handleSelectChange();
+  borderSize = toggleBorderSize();
+  localStorage.setItem("darkModeActive", isDarkMode);
+  location.reload();
 }
-function cancelExpense(btn) {
-  div = btn.parentNode;
-  cancelexpenseDescription = div.querySelector("#description").innerText;
-  cancelexpenseAmount = parseFloat(
-    div.querySelector("#expense-amount").innerText
-  );
-  totalbudget += cancelexpenseAmount;
-  totalexpense -= cancelexpenseAmount;
-  setHead();
-  setExpensesPer();
-  delete data.expenses[cancelexpenseDescription];
-  updateLocalStorage();
-  div.remove();
-}
-function updateLocalStorage() {
-  // quizArr = JSON.parse(window.localStorage.getItem("quizArr")) || quizArr;
-  localStorage.setItem("data", JSON.stringify(data));
-}
-function handleSelectChange() {
-  action = document.getElementById("action").value;
-  if (action == "add")
-    document.getElementById("submitAction").style.backgroundColor = "green";
-  else if (action == "reduce") {
-    document.getElementById("submitAction").style.backgroundColor = "red";
+function colorBody() {
+  if (isDarkMode) {
+    docBody.classList.add("darkMode");
+  } else {
+    docBody.classList.remove("darkMode");
   }
+}
+
+function updateDarkModeIcon() {
+  const darkModeToggler = document.querySelector(".darkModeToggler");
+  darkModeToggler.innerHTML = isDarkMode
+    ? '<i class="fas fa-sun darkModeButton"></i>'
+    : '<i class="fas fa-moon darkModeButton"></i>';
+}
+
+function changeDarkModeButtons() {
+  let checkDiv = document.querySelector(".checkmarkSwitch");
+  if (isDarkMode) {
+    checkDiv.innerHTML = `<i
+    class="fas fa-circle-check checkmark"
+    id="submitAction"
+    onclick="submitAction()"
+  ></i>`;
+  } else {
+    checkDiv.innerHTML = `<i
+    class="far fa-check-circle checkmark"
+    id="submitAction"
+    onclick="submitAction()"
+  ></i>`;
+  }
+}
+
+function toggleBorderSize() {
+  return isDarkMode ? "3px" : "2px";
 }
